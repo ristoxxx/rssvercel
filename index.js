3144fec80189ea582088c3befbf857ac8870731d
rlahteen <lahteri@gmail.com>
2025-02-08 14:04:37 +0200

    try fix

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ const feedUrl = 'https://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss'
 const parser = new RSSParser();
 let articles = [];
 
+async function fetchFeed() {
+    try {
+        const feed = await parser.parseURL(feedUrl);
+        articles = feed.items.map(item => ({
+            title: item.title,
+            link: item.link,
+            description: item.description
+        }));
+    } catch (error) {
+        console.error('Error fetching RSS feed:', error);
+        articles = []; // Fallback to empty array in case of failure
+    }
+}
+
+fetchFeed();
+
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 });
