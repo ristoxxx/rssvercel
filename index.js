ba9864fbffda9ec071c33534241ea9d204061289
rlahteen <lahteri@gmail.com>
2025-02-08 02:09:55 +0200

    try catch

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-/*
- * File: index.js
- * Created Date: Tuesday February 4th 2025 12:08:37
- * Author: ristoxxx@github.com
- * -----
- * Last Modified: Saturday February 8th 2025 02:02:47
- * Modified By: ristoxxx@github.com
- * -----
- * Copyright (c) 2025 ristoxxx@github.com
- */
 const express = require('express');
 const cors = require('cors');
 const RSSParser = require('rss-parser');
 
-
-const feedUrl = 'https://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss'
-
+const feedUrl = 'https://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss';
 const parser = new RSSParser();
 let articles = [];
 
-const feed = await parser.parseURL(feedUrl);
+async function fetchFeed() {
+    try {
+        const feed = await parser.parseURL(feedUrl);
+        articles = feed.items.map(item => ({
+            title: item.title,
+            link: item.link,
+            description: item.description
+        }));
+    } catch (error) {
+        console.error('Error fetching RSS feed:', error);
+        articles = []; // Fallback to empty array in case of failure
+    }
+}
 
-feed.items.forEach(item => {
-    articles.push({
-        title: item.title,
-        link: item.link,
-        description: item.description
-    });
-  });
+// Fetch the feed at startup
+fetchFeed();
 
 const app = express();
-
 app.use(cors());
 
 app.get('/articles', (req, res) => {
     res.json(articles);
 });
 
-// const server = app.listen("4000", () => {
-//     console.log("Server is running on ort 4000");
-// })
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-export default server;
-//hello
+
+module.exports = server;
