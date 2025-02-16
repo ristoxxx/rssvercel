9848d2f5937f6d331d0f70b0d5e0a3a8b0e5b803
rlahteen <lahteri@gmail.com>
2025-02-16 20:12:34 +0200

    major update, endpoints added and small change in logic

--- index.js
+++ index.js
@@ -1,41 +1,61 @@
 const express = require('express');
 const RSSParser = require('rss-parser');
 const cors = require('cors');
-const app = express();
 
+const app = express();
 app.use(cors());
+
 const PORT = 4000;
 
-const feedUrl = 'https://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss';
-const parser = new RSSParser();
-let articles = [];
+const feedUrls = {
+    yle: 'https://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss',
+    hs: 'http://www.hs.fi/rss/tuoreimmat.xml',
+    euro: 'https://europeannewsroom.com/feed/'
+};
 
-async function fetchFeed() {
+const parser = new RSSParser();
+
+async function fetchFeed(feedUrl) {
     try {
         const feed = await parser.parseURL(feedUrl);
-        articles = feed.items.map(item => ({
+        return feed.items.map(item => ({
             title: item.title,
             link: item.link,
             description: item.description
         }));
     } catch (error) {
         console.error('Error fetching RSS feed:', error);
-        articles = []; // Fallback to empty array in case of failure
+        return []; // Palautetaan tyhjä lista, jos haku epäonnistuu
     }
 }
 
-fetchFeed();
+// Asynkroninen reitti, joka odottaa `fetchFeed`-funktion valmistumista ennen vastaamista
+app.get('/yle', async (req, res) => {
+    const articles = await fetchFeed(feedUrls.yle);
+    res.status(200).json(articles);
+});
 
-app.get('/articles', (req, res) => {
+app.get('/articles', async (req, res) => {
+  const articles = await fetchFeed(feedUrls.yle);
   res.status(200).json(articles);
 });
 
+app.get('/hs', async (req, res) => {
+    const articles = await fetchFeed(feedUrls.hs);
+    res.status(200).json(articles);
+});
+
+app.get('/euro', async (req, res) => {
+    const articles = await fetchFeed(feedUrls.euro);
+    res.status(200).json(articles);
+});
+
 app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is maybe working well');
+    res.status(200).json('Welcome, your app is maybe working well');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
 
 module.exports = app;
